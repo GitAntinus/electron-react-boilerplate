1cde4e978afb06ab98eac29e429b201acffe343b
Eric Deng <gantinus@gmail.com>
2018-07-27 14:18:52 +0800

    Update installFont.js

--- styles/installFont.js
+++ styles/installFont.js
@@ -1,5 +1,14 @@
 const fs = require('fs');
 
+try {
+  if (!fs.statSync('files').isDirectory()) {
+    fs.unlinkSync('files');
+    fs.mkdirSync('files');
+  }
+} catch(e) {
+  fs.mkdirSync('files');
+}
+
 fs.readdir('node_modules', (err, fontdirs) => {
   for (let fontdir of fontdirs) {
     fs.copyFileSync(`node_modules/${fontdir}/index.css`, `${fontdir}.css`);
@@ -7,6 +16,20 @@ fs.readdir('node_modules', (err, fontdirs) => {
       for (let font of fonts) {
         fs.copyFileSync(`node_modules/${fontdir}/files/${font}`, `files/${font}`);
       }
+      deleteDir(`node_modules/${fontdir}`);
     })
   }
 });
+
+function deleteDir(path) {
+  const files = fs.readdirSync(path);
+  for (let file of files) {
+    let stat = fs.statSync(`${path}/${file}`);
+    if (stat.isDirectory()) {
+      deleteDir(`${path}/${file}`);
+    } else {
+      fs.unlinkSync(`${path}/${file}`);
+    }
+  }
+  fs.rmdirSync(path);
+}
