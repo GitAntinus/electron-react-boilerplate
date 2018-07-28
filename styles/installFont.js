002c523772582718373ae00804279afdd3f47f45
Eric Deng <gantinus@gmail.com>
2018-07-28 09:15:16 +0800

    Update installFont.js

--- styles/installFont.js
+++ styles/installFont.js
@@ -11,7 +11,7 @@ try {
 
 fs.readdir('node_modules', (err, fontdirs) => {
   for (let fontdir of fontdirs) {
-    fs.copyFileSync(`node_modules/${fontdir}/index.css`, `${fontdir}.css`);
+    fs.copyFileSync(`node_modules/${fontdir}/index.css`, `${fontdir}.less`);
     fs.readdir(`node_modules/${fontdir}/files`, (err, fonts) => {
       for (let font of fonts) {
         fs.copyFileSync(`node_modules/${fontdir}/files/${font}`, `files/${font}`);
